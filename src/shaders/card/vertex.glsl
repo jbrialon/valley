6e11685967d69bfb594212dfaec518aef971b984
Jérémy Brialon <jeremzor@gmail.com>
2024-02-04 17:02:34 +0100

    remove unused elements

--- src/shaders/card/vertex.glsl
+++
@@ -1,9 +0,0 @@
-varying vec2 vUv;
-varying vec3 uPosition;
-
-void main() {
-  vUv = uv;
-  uPosition = position;
-  vec4 modelViewPosition = modelViewMatrix * vec4(position, 1.0);
-  gl_Position = projectionMatrix * modelViewPosition;
-}
